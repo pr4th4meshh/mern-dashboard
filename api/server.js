d0b108dfa893c1b11dea7e81b83cd05a27d0f09c
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-06-14 20:20:42 +0530

    created user model, route, controller

--- api/server.js
+++ api/server.js
@@ -1,11 +1,27 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js";
+
+dotenv.config();
 
 const app = express()
+mongoose
+  .connect("mongodb+srv://prathamesh:prathamesh@cluster0.vzdiphv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    console.log("Connected to MongoDB!")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 
-app.listen(PORT, () => {console.log(`Server running on port:${PORT}`)})
+app.listen(5000, () => {
+  console.log(`Server running on port:${PORT}`)
+})
+
+app.use("/api/user", userRouter)
