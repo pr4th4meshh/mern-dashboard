5d3b012baffbdd97674a7f12a2eb8cf5fca76cc3
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-06-15 12:49:56 +0530

    Feature: Created error middleware

--- api/server.js
+++ api/server.js
@@ -22,6 +22,16 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+app.use((err, req,res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal Server Error'
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+})
+
 app.listen(5000, () => {
   console.log(`Server running on port:${PORT}`)
 })
