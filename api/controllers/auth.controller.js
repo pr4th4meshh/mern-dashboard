3881b7947770b5762fa5847879d6de5c5af12085
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-06-15 16:02:48 +0530

    Add: created signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,33 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body
   const hashedPassword = bcrypt.hashSync(password, 10)
   const newUser = new User({ username, email, password: hashedPassword })
   try {
     await newUser.save()
     res.status(201).json("New user created successfully")
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
+  try {
+    const validUser = await User.findOne({ email }) // find by email
+    if (!validUser) return next(errorHandler(404, "User not found"))
+    const validPassword = bcrypt.compareSync(password, validUser.password)
+    if (!validPassword) return next(errorHandler(404, "Invalid Credentials"))
+    const token = jwt.sign(
+      { id: validUser._id },
+      "thisismyjwtsecretkeyforthisapp"
+    )
+    const { password: pass, ...rest } = validUser._doc
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest)
   } catch (error) {
     next(error)
   }
--- api/server.js
+++ api/server.js
@@ -2,14 +2,16 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
-import userRouter from "./routes/user.route.js";
+import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 
-dotenv.config();
+dotenv.config()
 
 const app = express()
 mongoose
-  .connect("mongodb+srv://prathamesh:prathamesh@cluster0.vzdiphv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .connect(
+    "mongodb+srv://prathamesh:prathamesh@cluster0.vzdiphv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     console.log("Connected to MongoDB!")
   })
@@ -22,13 +24,13 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-app.use((err, req,res, next) => {
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
-  const message = err.message || 'Internal Server Error'
+  const message = err.message || "Internal Server Error"
   return res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   })
 })
 
