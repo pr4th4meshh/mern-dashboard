a1b0c51c8bd385c3aac25a7d4736c20efd11d11c
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-06-17 16:23:08 +0530

    Add: Added protected/private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Register from "./pages/register"
 import Login from "./pages/login"
 import MainLayout from "./components/layouts/MainLayout"
 import Profile from "./pages/profile"
+import PrivateRoute from "./utils/PrivateRoute"
 
 function App() {
   return (
@@ -12,10 +13,12 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<h1>Page not found :(</h1>} />
-      <Route element={<MainLayout />}>
+     <Route element={<PrivateRoute />} >
+     <Route element={<MainLayout />}>
         <Route path="/" element={<Events />} />
         <Route path="/profile" element={<Profile />} />
       </Route>
+     </Route>
     </Routes>
   )
 }
---
+++ client/src/utils/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux"
+import { Navigate, Outlet } from "react-router-dom"
+
+const PrivateRoute = () => {
+  const currentUser  = useSelector((state: unknown) => state.user.user)
+  return currentUser ? <Outlet /> : <Navigate to="/login" />
+}
+
+export default PrivateRoute
