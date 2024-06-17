88119efe2be15842e05c35d59d487586b1417d1d
pr4th4meshh <prathameshasolkar17@gmail.com>
2024-06-17 14:12:17 +0530

    Add: verifyToken w/ cookie-parser

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,23 +1,19 @@
 import jwt from "jsonwebtoken"
-import User from "../models/user.model";
+import User from "../models/user.model.js"
 
-const requireAuth = (roles) => {
-  return async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'Unauthorized' });
+export const verifyToken = (roles) => async (req, res, next) => {
+  const token = req.cookies.access_token
+  if (!token) return res.status(401).json({ message: "Unauthorized" })
 
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findById(decoded.id);
-      if (!roles.includes(user.role)) {
-        return res.status(403).json({ message: 'Forbidden' });
-      }
-      req.user = user;
-      next();
-    } catch (error) {
-      res.status(401).json({ message: 'Unauthorized' });
+  try {
+    const decoded = jwt.verify(token, "thisismyjwtsecretkeyforthisapp")
+    const user = await User.findById(decoded.id)
+    if (!roles.includes(user.role)) {
+      return res.status(403).json({ message: "Forbidden" })
     }
-  };
-};
-
-module.exports = requireAuth;
+    req.user = user
+    next()
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" })
+  }
+}
